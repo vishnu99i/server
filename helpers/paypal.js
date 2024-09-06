2aad1c545c014d04c86182dbfb3668b9d8857d54
Vishnu P <vinventor1999@gmail.com>
2024-09-06 16:37:38 +0530

    Paypal integration

--- helpers/paypal.js
+++ helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "",
-  client_secret: "",
+  client_id: "AYLYBbXKTTMpoRC41Dmwef8eZ4L8r4Cl_YQrUWTNyN0SDryn_VDMa-IFHYaH2yXMxin86aV2_MYUTq0_",
+  client_secret: "ENo_PhsHTgJqRc05o3r5i3D4dJLsrQQr9VdJoCB6tXiz-Yal9ev8OjKBtEbxAykSWjmKltzxagvNcYY7",
 });
 
 module.exports = paypal;
