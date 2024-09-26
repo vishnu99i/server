444cb87f8382874f9f4de9c86b4f3d847236dca0
Vishnu P <vinventor1999@gmail.com>
2024-09-26 20:08:40 +0530

    New commit

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const cookieParser = require("cookie-parser");
+//Cookies are small pieces of data that websites store in your browser
+//When you visit a website, the browser sends these cookies to the server so the site can "remember" you
+//cookie-parser turns cookies into a simple JavaScript object so server can easily read and use the data
+//Accessing Cookies => req.cookies
+//Signed Cookies(have security key) => req.singnedCookies
 const cors = require("cors");
 
 const authRouter = require("./routes/auth/auth-routes");
@@ -31,6 +36,8 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
+//mongoose.connect() will return a promise
+
   // mongodb+srv://vinventor1999:passwordmernproject123@cluster0.qmugk.mongodb.net/
 
 const app = express();
@@ -41,18 +48,19 @@ app.use(
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
+      "Content-Type", //Type of content sent in request body
+      "Authorization", //Used to sent authentication tokens
+      "Cache-Control", //Helps control how responses are cached on the client side and Cache is a place where frequently used information is temporarily stored so it can be quickly accessed later
+      "Expires", //This header specifies an expiration date for the cached resource
+      "Pragma", //Similar to Cache-Control, but mainly for backward compatibility with older HTTP/1.0 caches
     ],
     credentials: true,
   })
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json()); //Incoming JSON data from requests to Javascript Object
+
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
