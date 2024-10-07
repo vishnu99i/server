9d3925102864f58e4b524c5fd13b764660c07e70
Vishnu P <vinventor1999@gmail.com>
2024-10-07 14:24:07 +0530

    Latest commit

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://client-app-one-omega.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type", //Type of content sent in request body
