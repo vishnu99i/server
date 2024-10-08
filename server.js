e67b700e1fce52bd15de9a0c28e0941eede2160a
Vishnu P <vinventor1999@gmail.com>
2024-10-08 14:23:20 +0530

    New commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 //Cookies are small pieces of data that websites store in your browser
 //When you visit a website, the browser sends these cookies to the server so the site can "remember" you
 //cookie-parser turns cookies into a simple JavaScript object so server can easily read and use the data
 //Accessing Cookies => req.cookies
 //Signed Cookies(have security key) => req.singnedCookies
-const cors = require("cors");
 // const dotenv = require("dotenv");
 // dotenv.config({path:"./.env"});
 
@@ -47,25 +47,31 @@ mongoose
 
   // mongodb+srv://vinventor1999:passwordmernproject123@cluster0.qmugk.mongodb.net/
 
-const app = express();
 // const PORT = process.env.PORT || 5000;
 
-// app.use(
-//   cors({
-//     origin: "https://client-app-one-omega.vercel.app",
-//     methods: ["GET", "POST", "DELETE", "PUT"],
-//     allowedHeaders: [
-//       "Content-Type", //Type of content sent in request body
-//       "Authorization", //Used to sent authentication tokens
-//       "Cache-Control", //Helps control how responses are cached on the client side and Cache is a place where frequently used information is temporarily stored so it can be quickly accessed later
-//       "Expires", //This header specifies an expiration date for the cached resource
-//       "Pragma", //Similar to Cache-Control, but mainly for backward compatibility with older HTTP/1.0 caches
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://client-app-one-omega.vercel.app",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type", //Type of content sent in request body
+      "Authorization", //Used to sent authentication tokens
+      "Cache-Control", //Helps control how responses are cached on the client side and Cache is a place where frequently used information is temporarily stored so it can be quickly accessed later
+      "Expires", //This header specifies an expiration date for the cached resource
+      "Pragma", //Similar to Cache-Control, but mainly for backward compatibility with older HTTP/1.0 caches
+    ],
+    credentials: true,
+  })
+);
 
-app.use(cors());
+// https://client-app-one-omega.vercel.app
+// app.use(cors());
+// const corsOptions = {
+//   origin: 'http://localhost:5173',
+//   optionsSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json()); //Incoming JSON data from requests to Javascript Object
@@ -85,6 +91,6 @@ app.use("/api/common/feature", commonFeatureRouter);
 
 // app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
 
-export default app
+// export default app
 
-// module.exports = app; // Instead of app.listen()
+module.exports = app; // Instead of app.listen()
